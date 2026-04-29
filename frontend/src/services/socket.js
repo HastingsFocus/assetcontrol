418d6ab3428940eb284cc73bee736c6e2efc1d5c
Hastings Focus <bit-019-21@must.ac.mw>
2026-04-29 16:36:08 +0200

    yeah yeah

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,12 +1,13 @@
 import { io } from "socket.io-client";
 
 const URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
-console.log("🔌 SOCKET URL:", URL); // 👈 VERY IMPORTANT DEBUG
+console.log("🔌 SOCKET URL:", URL);
 
 const socket = io(URL, {
   withCredentials: true,
+  transports: ["websocket"], 
 });
 
 export default socket;

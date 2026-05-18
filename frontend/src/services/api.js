40023ac4391e396a18b5221990ec16a232df29f9
Hastings Focus <bit-019-21@must.ac.mw>
2026-05-18 14:31:54 +0200

    Demo version for lecturers (notifications feature)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // 🔥 force local backend
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = sessionStorage.getItem("token");
 
